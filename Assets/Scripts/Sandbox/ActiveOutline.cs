cea76dcd5b6721f97d8d1d24a8bca92c5f165eef
Gerome Matilla <gerome.matilla07@gmail.com>
2019-04-22 23:46:51 +0800

    ActiveOutline Update

--- Assets/Scripts/Sandbox/ActiveOutline.cs
+++ Assets/Scripts/Sandbox/ActiveOutline.cs
@@ -2,85 +2,113 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActiveOutline : MonoBehaviour {
-	private Color origOutline;
-	private Color activeOutline;
-	private GameObject oldActive;
-	private Renderer tree;
-	private bool isTree;
-	public bool activateOutline = false;
-	private int numOfChild;
-	// Use this for initialization
-	void Start () {
-		origOutline = new Color (0f, 0f, 0f, 1f);
-		activeOutline = new Color (255f, 0f, 0f, 0f);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (activateOutline) {
+public class ActiveOutline : MonoBehaviour
+{
+    private GameObject oldActive;
+    public bool activateOutline = false;
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-			if (Input.touchCount > 0) {
-				Touch touch = Input.touches [0];
-				Ray ray = Camera.main.ScreenPointToRay (touch.position);
-				RaycastHit hit;
+    // Update is called once per frame
+    void Update()
+    {
+        if (activateOutline)
+        {
 
-				if (Physics.Raycast (ray, out hit)) {
-			
-					if (oldActive != hit.collider.gameObject && oldActive != null && touch.phase == TouchPhase.Began) {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.touches[0];
+                Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                RaycastHit hit;
 
-						oldActive.GetComponent<Renderer> ().material.SetColor ("_OutlineColor", origOutline);
+                if (Physics.Raycast(ray, out hit))
+                {
 
-					}
+                    if ( oldActive && touch.phase == TouchPhase.Began)
+                    {
 
-					if ((hit.collider.tag == "Sands" || hit.collider.tag == "Model") && touch.phase == TouchPhase.Began) {
+                        if (oldActive.transform.childCount < 1)
+                        {
+                            oldActive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.black);
+                        }
+                        else
+                        {
 
+                            foreach (Renderer r in oldActive.GetComponentsInChildren<Renderer>())
+                            {
 
-						if (hit.collider.transform.childCount > 0) {
-							
-							OutlineTree ();
-							hit.collider.transform.Find ("tree-baobab").GetComponent<Renderer> ().material.SetColor ("_OutlineColor", activeOutline);
-							tree = hit.collider.transform.Find ("tree-baobab").GetComponent<Renderer> ();
-							isTree = true;
+                                r.material.SetColor("_OutlineColor", Color.black);
+                            }
 
-						} else {
-
-							hit.collider.gameObject.GetComponent<Renderer> ().material.SetColor ("_OutlineColor", activeOutline);
-							oldActive = hit.collider.gameObject;
-							OutlineTree ();
-						}
-
-					}
-
-				}
-
-			}
-
-		}
-
-	}
-
-	public void NormalOutline(){
-	
-		if (oldActive) {
-			oldActive.GetComponent<Renderer> ().material.SetColor ("_OutlineColor", origOutline);
-			OutlineTree ();
-			activateOutline = false;
-		}
-	}
-
-	public void StartOutline(){
-	
-		activateOutline = true;
-	}
+                        }
 
 
-	private void OutlineTree(){
-		if (isTree) {
-			tree.material.SetColor ("_OutlineColor", origOutline);
-			isTree = false;
-		}
+                    }
 
-	}
+                    if ((hit.collider.tag == "Sands" || hit.collider.tag == "Model") && touch.phase == TouchPhase.Began)
+                    {
+
+
+                        if (hit.collider.gameObject.transform.childCount == 0)
+                        {
+                            hit.collider.gameObject.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.red);
+                            oldActive = hit.collider.gameObject;
+                        }
+                        else
+                        {
+
+                            foreach (Renderer r in hit.collider.gameObject.GetComponentsInChildren<Renderer>())
+                            {
+
+                                r.material.SetColor("_OutlineColor", Color.red);
+
+                            }
+                            oldActive = hit.collider.gameObject;
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+    }
+
+    public void NormalOutline()
+    {
+
+        if (oldActive)
+        {
+            if (oldActive.transform.childCount < 1)
+            {
+                oldActive.GetComponent<Renderer>().material.SetColor("_OutlineColor", Color.black);
+            }
+            else
+            {
+
+                foreach (Renderer r in oldActive.GetComponentsInChildren<Renderer>())
+                {
+
+                    r.material.SetColor("_OutlineColor", Color.black);
+                }
+
+            }
+
+
+        }
+    }
+
+    public void StartOutline()
+    {
+
+        activateOutline = true;
+    }
+
+    
 
 }
