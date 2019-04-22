72c50c8e73e26558d5ba6eddc193b9c93021087e
Gerome Matilla <gerome.matilla07@gmail.com>
2019-04-22 09:37:10 +0800

    updat logo and forest prefabs

--- Assets/Scripts/OpenPDF.cs
+++ Assets/Scripts/OpenPDF.cs
@@ -10,12 +10,36 @@ public class OpenPDF : MonoBehaviour {
     
     public void openBook()
     {
-        string path = System.IO.Path.Combine(Application.persistentDataPath, bookName + ".pdf");
 
-        TextAsset pdfTemp = Resources.Load(bookName, typeof(TextAsset)) as TextAsset;
-        File.WriteAllBytes(path, pdfTemp.bytes);
-        Application.OpenURL(path);
+        if (getSDKInt() > 23) {
+
+            Application.OpenURL("http://facebook.com");
+                
+        }
+        else
+        {
+
+            string path = System.IO.Path.Combine(Application.persistentDataPath, bookName + ".pdf");
+
+            TextAsset pdfTemp = Resources.Load(bookName, typeof(TextAsset)) as TextAsset;
+            File.WriteAllBytes(path, pdfTemp.bytes);
+            Application.OpenURL(path);
+        }
         
     }
+
+    static int getSDKInt()
+    {
+
+#if UNITY_ANDROID && !UNITY_EDITOR
+        using (var version = new AndroidJavaClass("android.os.Build$VERSION"))
+        {
+            return version.GetStatic<int>("SDK_INT");
+        }
+#endif
+#if UNITY_EDITOR
+        return 23;
+#endif
+    }
+
 }
-    
